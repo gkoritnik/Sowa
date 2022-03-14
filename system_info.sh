c5045270e8d25d7eeeb30af008ef3a0b31e1da8a
Gregor <koritnik@iskratel.si>
2022-03-14 13:30:07 +0000

    Added comments in system info script.

--- system_info.sh
+++ system_info.sh
@@ -5,13 +5,18 @@ sys_log_file="system-info.log"
 # If logfile doesn't exists. 
 if [ ! -f $sys_log_file ]
   then
-    printf "RAM\t\tDisk\t\tCPU\n" > $sys_log_file
+    printf "Memory\t\tDisk\t\tCPU\n" > $sys_log_file
 fi
 
 
-# Store MEM, DISK and CPU.
+# Store MEM value. Awk reads second line, used_mem*100/total_mem . 
 MEM=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+
+# Store DISK value. Awk reads / (root) partition and stores 5th column.
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+
+# Store CPU value. Top is called once, awk reads first column after load average:.  
 CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 
+# Store collected values to file.
 echo "$MEM$DISK$CPU" >> $sys_log_file
