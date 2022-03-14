f961f596a9d3c27aaff304c33d5683a0115bbea7
Gregor <koritnik@iskratel.si>
2022-03-14 12:59:58 +0000

    Update on System_info script.

--- system_info.sh
+++ system_info.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
-printf "Memory\t\tDisk\t\tCPU\n"
 
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+sys_log_file="system-info.log"
+
+# If logfile doesn't exists. 
+if [ ! -f $sys_log_file ]
+  then
+    printf "Memory\t\tDisk\t\tCPU\n" > $sys_log_file
+fi
+
+
+# Store MEM, DISK and CPU.
+MEM=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
-echo "$MEMORY$DISK$CPU" > system-info.log
+
+echo "$MEM$DISK$CPU" >> $sys_log_file
