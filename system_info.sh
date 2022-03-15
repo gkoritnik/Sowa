677c18f07446c0c0f4b5c15e8edb94a54e0d500a
Gregor Koritnik <koritnik.gregor@gmail.com>
2022-03-15 14:39:57 +0100

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -16,7 +16,7 @@ MEM=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 
 # Store CPU value. Top is called once, awk reads first column after load average:.  
-CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
 
 # Store collected values to file.
 echo "$MEM$DISK$CPU" >> $sys_log_file
