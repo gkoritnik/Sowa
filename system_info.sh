b6a25b06d1bdbe4741a3a23c5b7c23a4678477a5
Gregor Koritnik <koritnik.gregor@gmail.com>
2022-03-15 15:05:58 +0100

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -2,21 +2,26 @@
 
 sys_log_file="system-info.log"
 
-# If logfile doesn't exists. 
+# If logfile doesn't exists.
 if [ ! -f $sys_log_file ]
   then
-    printf "Memory\t\tDisk\t\tCPU\n" > $sys_log_file
+    printf "Memory,Disk,CPU\n" > $sys_log_file
 fi
 
+while true
+do
 
-# Store MEM value. Awk reads second line, used_mem*100/total_mem . 
-MEM=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+# Store MEM value. Awk reads second line, used_mem*100/total_mem .
+MEM=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
 
 # Store DISK value. Awk reads / (root) partition and stores 5th column.
-DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+DISK=$(df -h | awk '$NF=="/"{printf "%s", $5}')
 
-# Store CPU value. Top is called once, awk reads first column after load average:.  
+# Store CPU value. Top is called once, awk reads first column after load average:.
 CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
 
 # Store collected values to file.
-echo "$MEM$DISK$CPU" >> $sys_log_file
+echo "$MEM","$DISK","$CPU" >> $sys_log_file
+
+sleep 10
+done
